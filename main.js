68e8497536106bb91f040a2ef1fa94d8163c87a8
遐思遥爱 <44915671+SuenWangzhi@users.noreply.github.com>
2021-01-30 22:15:17 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,155 @@
-const hashMap = {}
+const $siteList = $('.siteList')
+const $lastLi = $("ul li:last-child")
+const websites = JSON.parse(localStorage.getItem('websites'))
+const pageStyle = JSON.parse(localStorage.getItem('pageStyle'))
+const hashMap = websites || [
+    {
+        img: '//static.hdslb.com/mobile/img/512.png',
+        url: '//bilibili.com',
+        name: '哔哩哔哩',
+        bgColor: '1'
+    },
+    {
+        img: 'https://static.zhihu.com/heifetz/assets/apple-touch-icon-120.bbce8f18.png',
+        url: '//zhihu.com',
+        name: '知乎',
+        bgColor: '1'
+    },
+    {
+        img: 'https://b-gold-cdn.xitu.io/favicons/v2/apple-touch-icon.png',
+        url: '//juejin.cn',
+        name: '掘金',
+        bgColor: '1'
+    },
+    {
+        img: 'https://gw.alipayobjects.com/mdn/prod_resou/afts/img/A*CUIoT4xopNYAAAAAAAAAAABkARQnAQ',
+        url: '//yuque.com',
+        name: '语雀',
+        bgColor: '1'
+    },
+    {
+        url: '//github.com',
+        name: 'GitHub',
+        bgColor: '12'
+    }
+]
+const render = () => {
+    $siteList.find('li:not(#lastLi)').remove()
+    hashMap.forEach((node, index) => {
+        if (!node.img) {
+            const $li = $(`<li class="box">
+                <a href="//${node.url}" target="_blank" class="img">
+                    <div class="website-icon color${node.bgColor}">
+                        ${node.name[0]}
+                    </div>
+                </a>
+                <svg class="delete-icon">
+                <use xlink:href="#icon-delete"></use>
+            </svg>
+                <p class="website-name">${node.name}</p>
+           
+        </li>`).insertBefore($lastLi)
+            $li.on('click', '.delete-icon', (e) => {
+                hashMap.splice(index, 1)
+                localStorage.setItem('websites', JSON.stringify(hashMap))
+                render()
+            });
+        } else {
+            const $li = $(`<li class="box">
+            <a href="${node.url}" target="_blank" class="img">
+                <img class="website-icon"
+                src="${node.img}" />
+            </a>
+            <svg class="delete-icon">
+                <use xlink:href="#icon-delete"></use>
+            </svg>
+            <p class="website-name">${node.name}</p>
+           
+        </li>`).insertBefore($lastLi)
+            $li.on('click', '.delete-icon', (e) => {
+                hashMap.splice(index, 1)
+                localStorage.setItem('websites', JSON.stringify(hashMap))
+                render()
+            });
+        }
+    })
+}
+render()
+$('#lastLi').on('click', () => {
+    let url = window.prompt('网站地址')
+    let name = window.prompt('网站名称')
+    const bgColor = parseInt(Math.random() * 16 + 1);
+    hashMap.push({
+        url: url,
+        name: name,
+        bgColor: bgColor
+    })
+    render()
+    localStorage.setItem('websites', JSON.stringify(hashMap))
+})
 $("input").focus(function () {
     $("input").css('background', '#ffffff')
     $(".kw-wrap").css('background', '#ffffff')
 })
-
 $("input").blur(function () {
     $("input").css('background', 'rgba(255,255,255,0)')
     $(".kw-wrap").css('background', 'rgba(255,255,255,0)')
 })
+let a = pageStyle || 1;
+const dog = () => {
+    if (a === 1) {
+        $(".moon").css('display', 'inline')
+        $(".sun").css('display', 'none')
+        $('link[name="pageStyle"]').attr('href', './css/default.css');
+        $('.setting').html("拟态风格")
+    } else if (a === 2) {
+        $(".moon").css('display', 'none')
+        $(".sun").css('display', 'inline')
+        $('link[name="pageStyle"]').attr('href', './css/dark-default.css');
+        $('.setting').html("拟态风格")
+    } else if (a === 3) {
+        $(".moon").css('display', 'inline')
+        $(".sun").css('display', 'none')
+        $('link[name="pageStyle"]').attr('href', './css/nitai.css');
+        $('.setting').html("原生风格")
+    } else if (a === 4) {
+        $(".moon").css('display', 'none')
+        $(".sun").css('display', 'inline')
+        $('link[name="pageStyle"]').attr('href', './css/dark-nitai.css');
+        $('.setting').html("原生风格")
+    }
+}
+dog()
 $('.moon').click(function () {
-    $('link[href="./css/nitai.css"]').attr('href', './css/dark-nitai.css');
-    $('link[href="./css/default.css"]').attr('href', './css/dark-default.css');
-    $(".moon").css('display', 'none')
-    $(".sun").css('display', 'inline')
+    if (a === 1) {
+        a = 2
+    } else (
+        a = 4
+    )
+    localStorage.setItem('pageStyle', JSON.stringify(a))
+    dog()
 });
 $('.sun').click(function () {
-    $('link[href="./css/dark-nitai.css"]').attr('href', './css/nitai.css');
-    $('link[href="./css/dark-default.css"]').attr('href', './css/default.css');
-    $(".sun").css('display', 'none')
-    $(".moon").css('display', 'inline')
+    if (a === 2) {
+        a = 1
+    } else (
+        a = 3
+    )
+    localStorage.setItem('pageStyle', JSON.stringify(a))
+    dog()
 });
-let a = 1;
 $('.setting').click(function () {
     if (a === 1) {
-        $('link[href="./css/default.css"]').attr('href', './css/nitai.css')
-        $('link[href="./css/dark-default.css"]').attr('href', './css/dark-nitai.css')
-        a = 0
-    } else {
-        $('link[href="./css/nitai.css"]').attr('href', './css/default.css')
-        $('link[href="./css/dark-nitai.css"]').attr('href', './css/dark-default.css')
+        a = 3
+    } else if (a === 2) {
+        a = 4
+    } else if (a === 3) {
         a = 1
+    } else {
+        a = 2
     }
+    localStorage.setItem('pageStyle', JSON.stringify(a))
+    dog()
 });
 let b = 1;
 $('.btn-change').click(function () {
@@ -56,24 +175,14 @@ $('.btn-change').click(function () {
         b = 1
     }
 });
-$('#addButton').on('click', () => {
-    let url = window.prompt('网站地址')
-    let name = window.prompt('网站名称')
-    let icon = window.prompt('显示名称')
-    const $siteList = $('.siteList')
-    const $lastLi = $("ul li:last-child")
-    const bgColor = parseInt(Math.random() * 16 + 1);
-    const $lia = $(`<li class="box">
-    <a class="href" href="${url}">
-        <div class="img">
-            <div class="website-icon color${bgColor}">
-                ${icon}
-            </div>
-        </div>
-        <p class="website-name">${name}</p>
-    </a>
-</li>s`).insertBefore($lastLi)
-    console.log("aaa:", $lastLi)
+document.oncontextmenu = function () {
+    // return false;
+}
+$(".img").mousedown(function (e) {
+    //右键为3
+    if (3 == e.which) {
+        console.log("e.which");
+    } else if (1 == e.which) {
+    }
 })
-
 
